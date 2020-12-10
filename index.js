c092ab467e6997808410f685d23ffb5a33950c79
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-12-10 17:54:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,4 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased= tutorials =>  tutorials.map(sentence => sentence.split(" ").map(word => word.charAt(0).toUpperCase()+word.slice(1)).join(" "))
